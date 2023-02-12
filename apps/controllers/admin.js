cf2baa2417637a3f8d566278a42fc45d4c149e93
Nguyen Anh Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-02-12 23:55:15 +0700

    design sigun up form

--- apps/controllers/admin.js
+++ apps/controllers/admin.js
@@ -8,4 +8,8 @@ router.get("/", function(req, res){
     res.json({"message": "This is Admin Page"});
 });
 
+router.get("/signup", function(req, res){
+    res.render("signup");
+});
+
 module.exports = router;
